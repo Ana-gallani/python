60da3ca7a52f65c7ef1ab2b9522b862866c33eef
Ana <ana.gallani4@gmail.com>
2023-01-05 01:54:22 +0530

    python basics done

---
+++ exercise#10.py
@@ -0,0 +1,10 @@
+subjects = ["English", "Hindi", "Maths", "Science"]
+
+for subject in subjects:
+    print(subject)
+
+s = 0
+while s < len(subjects):
+    print(subjects[s])
+    s = s+1
+
---
+++ exercixe#9.py
@@ -0,0 +1,6 @@
+#print a pattern using while loop
+
+i = 10
+while i >= 0:
+    print(i*"*")
+    i = i-1
